b3e8ee94974c6d108ca0c95dda8be929d4eb73d6
Maxime Brossard <maxime.brossard5@gmail.com>
2024-10-18 11:25:16 +0200

    feat: add unit test to app

--- asi-avi-rest-api/Controllers/UtilisateursController.cs
+++ asi-avi-rest-api/Controllers/UtilisateursController.cs
@@ -87,6 +87,10 @@ namespace asi_avi_rest_api.Controllers
         [HttpPost]
         public async Task<ActionResult<Utilisateur>> PostUtilisateur(Utilisateur utilisateur)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             _context.Utilisateurs.Add(utilisateur);
             await _context.SaveChangesAsync();
 
---
+++ asi-avi-rest-apiTests/Controllers/UtilisateursControllerTests.cs
@@ -0,0 +1,100 @@
+﻿using asi_avi_rest_api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.ContentModel;
+using NuGet.Protocol;
+using System.Collections.Generic;
+using System.Xml;
+using TP2Console.Models.EntityFramework;
+
+namespace asi_avi_rest_apiTests.Controllers
+{
+    [TestClass]
+    public class UtilisateursControllerTests
+    {
+        private readonly NotationDbContext _context;
+        private UtilisateursController controller;
+       public UtilisateursControllerTests() 
+        {
+            var builder = new DbContextOptionsBuilder<NotationDbContext>()
+                .UseNpgsql("Server=localhost;port=5432;Database=NotationDB;uid=postgres;password=postgres;");
+
+            _context = new NotationDbContext(builder.Options);
+            controller = new UtilisateursController(_context);
+        }
+
+        [TestMethod]
+        public async Task GetAllUtilisateurs_CompareWithDB()
+        {
+            var result = await controller.GetUtilisateurs();
+            var userInDB = _context.Utilisateurs.ToList();
+            
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(userInDB.Count, result.Value.Count());
+        }
+
+        [TestMethod]
+        public async Task GetByID_SuccessGetUserByID()
+        {
+            var result = await controller.GetUtilisateurById(1);
+            var userInDB = _context.Utilisateurs.Where(c=>c.Idutilisateur == 1).FirstOrDefault();
+
+            Assert.AreEqual(userInDB.Mail, result.Value.Mail);
+        }
+
+        [TestMethod]
+        public async Task GetByID_FailureGetUserByID()
+        {
+            var result = await controller.GetUtilisateurById(100000);
+
+            Assert.AreEqual(result.Value, null);
+        }
+
+        [TestMethod]
+        public async Task GetByEmail_SuccessGetUserByEmail()
+        {
+            var result = await controller.GetUtilisateurByEmail("clilleymd@last.fm");
+            var userInDB = _context.Utilisateurs.Where(c => c.Mail == "clilleymd@last.fm").FirstOrDefault();
+
+            Assert.AreEqual(userInDB.Idutilisateur, result.Value.Idutilisateur);
+        }
+
+        [TestMethod]
+        public async Task GetByEmail_FailureGetUserByEmail()
+        {
+            var result = await controller.GetUtilisateurByEmail("test@test.com");
+
+            Assert.AreEqual(result.Value, null);
+        }
+
+        [TestMethod]
+        public void Postutilisateur_ModelValidated_CreationOK()
+        {
+            Random rnd = new Random();
+            int chiffre = rnd.Next(1, 1000000000);
+            Utilisateur userAtester = new Utilisateur()
+            {
+                Idutilisateur = chiffre,
+                Nom = "MACHIN",
+                Prenom = "Luc",
+                Mobile = "0606070809",
+                Mail = "machin" + chiffre + "@gmail.com",
+                Pwd = "Toto1234!",
+                rue = "Chemin de Bellevue",
+                CodePostal = "74940",
+                Ville = "Annecy-le-Vieux",
+                Pays = "France",
+                Latitude = null,
+                Longitude = null
+            };
+            // Act
+            var result = controller.PostUtilisateur(userAtester).Result;
+            // Assert
+            Utilisateur? userRecupere = _context.Utilisateurs.Where(u => u.Mail.ToUpper() ==
+            userAtester.Mail.ToUpper()).FirstOrDefault();
+            userAtester.Idutilisateur = userRecupere.Idutilisateur;
+            Assert.AreEqual(userRecupere, userAtester, "Utilisateurs pas identiques");
+        }
+    }
+}
